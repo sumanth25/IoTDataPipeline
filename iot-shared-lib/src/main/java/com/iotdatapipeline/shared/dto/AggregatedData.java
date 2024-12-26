270a3d4f40f65f3cc7f34e34cd731637a2326c73
sumanth25 <sumanth.sudeendra@gmail.com>
2024-12-26 11:43:35 +0530

    Update shared library

--- iot-shared-lib/src/main/java/com/iotdatapipeline/shared/dto/AggregatedData.java
+++ iot-shared-lib/src/main/java/com/iotdatapipeline/shared/dto/AggregatedData.java
@@ -1,8 +1,12 @@
 package com.iotdatapipeline.shared.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AggregatedData {
     private double avgValue;
     private double minValue;
--- iot-shared-lib/src/main/java/com/iotdatapipeline/shared/dto/DeviceDTO.java
+++ iot-shared-lib/src/main/java/com/iotdatapipeline/shared/dto/DeviceDTO.java
@@ -1,11 +1,17 @@
 package com.iotdatapipeline.shared.dto;
-
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class DeviceDTO {
     private String id;
     private String deviceType;
+    @JsonIgnore
+    @JsonProperty
+    private long createdAt;
 }
